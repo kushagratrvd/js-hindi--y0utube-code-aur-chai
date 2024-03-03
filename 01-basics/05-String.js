a6291bc42a4f7dc8884980ad0c33b4ea706dd83f
kushagratrvd <146850405+kushagratrvd@users.noreply.github.com>
2024-03-03 10:47:20 +0000

    Math and Num

---
+++ 01-basics/05-String.js
@@ -0,0 +1,17 @@
+
+const name = "Kushagra"
+const name2 = "Chai"
+console.log(`My name is ${name} and i am drinking ${name2}`)
+const gameName = new String('Kushahgra-Trivedi-js')
+console.log(gameName[9])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.toLowerCase())
+const AnotherString  = gameName.slice(3,4)
+console.log(AnotherString);
+const OtherString = gameName.substring(1,4)
+console.log(OtherString)
+console.log(gameName.split('-'))
+const url = "google@kushagra-trivedi.com"
+console.log(url.replace('-',' '))
---
+++ 01-basics/06-nums-and-maths.js
@@ -0,0 +1,18 @@
+const n1 = 400
+console.log(n1)
+const n2 = new Number(400)
+console.log(n2)
+console.log(n2.toString())
+console.log(n2.toString().length)
+const n3 = 123.8898
+console.log(n3.toPrecision(3))
+//+++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++
+console.log(Math.abs(-4))
+console.log(Math.min(1,2,3,4))
+console.log(Math.max(2,3,4,56,66))
+console.log(Math.floor(2.9))
+console.log(Math.ceil(2.1))
+console.log(Math.random())
+const min =10;
+const max = 20;
+console.log(Math.floor(Math.random()*(10) + min))
