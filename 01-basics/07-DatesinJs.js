cb5e0cd8416791410e47c4490e5d08094749b7a0
kushagratrvd <146850405+kushagratrvd@users.noreply.github.com>
2024-03-03 11:26:54 +0000

    DateinJs

---
+++ 01-basics/07-DatesinJs.js
@@ -0,0 +1,8 @@
+const myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.getDate())
+console.log(typeof myDate)
