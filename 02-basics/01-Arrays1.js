66c24b4dd9edcfe4a9b2dfed352951770a9a4caf
kushagratrvd <146850405+kushagratrvd@users.noreply.github.com>
2024-03-05 11:49:27 +0000

    Arrays1

---
+++ 02-basics/01-Arrays1.js
@@ -0,0 +1,21 @@
+//const arr = [0,1,2,3,4]
+const arr1 = new Array(1,2,3,4,5)
+//arr1.push(6)
+//console.log(arr1)
+//arr1.unshift(9)
+//console.log(arr1)
+//arr1.shift()
+//console.log(arr1)
+//console.log(arr1.indexOf(2))
+//const arr2 = arr1.join()
+//console.log(arr2) 
+console.log("a ", arr1) 
+
+const arr3 = arr1.slice(1,3)
+console.log(arr3) 
+console.log("b ",arr1) 
+
+const arr4 = arr1.splice(1,3)
+console.log(arr4)
+console.log("c ",arr1)
+
